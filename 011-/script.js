bfbec52bedfec274d6522ea0d5621339f3313048
nikitos4628 <soba.nikita@mail.ru>
2023-02-09 20:37:00 +0700

    still polishing

---
+++ 011-/script.js
@@ -0,0 +1,104 @@
+"use strict"
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+// 	const result = 50;
+	
+// }
+
+// console.log(result);
+
+// // 
+
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = `5`;
+
+// let number1 = 10;
+// console.log(2 + 2);
+
+// {
+// 	var result = 50 ;
+// }
+
+
+// const bool = false;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+// 	name1: "John",
+// 	age: 25,
+// 	isMarried: false
+// };
+
+// // console.log(obj.name1);
+// console.log(obj ["name1"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+// // 
+
+// // alert('hello');
+
+// const result = confirm("Are you here?");
+// console.log(result); 
+
+// const answer = +prompt('Вам есть 18?', '18')
+// console.log(answer + 5); 
+
+// const answers = [];
+
+// answers[0] = prompt('Какое ваше имя?', '')
+// answers[1] = prompt('Какая ваша фамилия?', '')
+// answers[2] = prompt('Сколько вам лет?', '')
+
+// console.log(typeof(answers)); 
+// console.log(typeof(null));
+
+// 
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`);
+
+// 
+
+console.log('arr' + " object");
+
+console.log(4 + + "5");
+
+let incr = 10,
+	decr = 10;
+
+	// ++incr;
+	// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log(2*4 === '8');
+
+const isChecked = true,
+	isClosed = false;
+
+console.log(isChecked || !isClosed);
+
+console.log(2 + 2 * 2 === 8);
