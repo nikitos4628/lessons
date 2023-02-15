31e5c8d1c933b983c5e5e48c7dba9aa14aa9ebd6
nikitos4628 <soba.nikita@mail.ru>
2023-02-15 19:08:57 +0700

    continue udemy, same day

--- u33/script.js
+++ u33/script.js
@@ -1,1 +1,37 @@
-'use strict';
+// 'use strict';
+
+const arr = [323,121,2,3,6,7];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+	return a - b; 
+}
+
+// // arr[99] = 0;
+// // console.log(arr.length);
+// // console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+// 	console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// // arr.pop(); 
+// // arr.push(10);
+
+// // console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+// 	console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+// 	console.log(value);
+// }
+
+// const str = prompt('', '');
+// const products = str.split(', ');
+// // console.log(products);
+// products.sort();
+// console.log(products.join('; '));
+
---
+++ u35/script.js
@@ -0,0 +1,51 @@
+'strict';
+
+let a = 5,
+	b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+	a: 5,
+	b: 1
+};
+
+const copy = obj; //Ссылку
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj);
+
+
+
+function copy1(mainObj) {
+	let objCopy = {};
+
+	let key;
+	for (key in mainObj) {
+		objCopy[key] = mainObj[key];
+	}
+
+	return objCopy;
+}
+
+const numbers = {
+	a: 2,
+	b: 5,
+	c: {
+		x: 7,
+		y: 4
+	}
+};
+
+const newNumbers = copy1(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);
