9b0c535dac6369678976efd5ae35ba9176f09de0
nikitos4628 <soba.nikita@mail.ru>
2023-02-17 08:34:30 +0700

    continue udemy 17.02

---
+++ u41/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// let x = 5; alert( ++x );
+
+// [ ] + false - null + true;
+// console.log([] + false - null + true);
+
+// let y = 1; let x = y = 2; alert(x);
+
+// [ ] + 1 + 2;
+// console.log([ ] + 1 + 2);
+
+// alert('1'[0]);
+// console.log('1'[0]);
+
+// 2 && 1 && null && 0 && undefined;
+// console.log(2 && 1 && null && 0 && undefined);
+
+// !!( a && b) и (a && b);
+// console.log(!!( 1 && 2) === (1 && 2));
+
+// alert( null || 2 && 3 || 4 );
+
+// const a = [1, 2, 3]; 
+// const b = [1, 2, 3]; 
+// console.log(a == b);
+
+// alert(+'Infinity');
+
+// console.log('asd' > 'saa');
+
+console.log(0 || '' || 2 || undefined || true || falsе);
