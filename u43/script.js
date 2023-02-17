6d7f72d9eb04cb6c1b463c5bb8eb8e729c01db52
nikitos4628 <soba.nikita@mail.ru>
2023-02-17 12:31:34 +0700

    continue udemy, 1 day with css

---
+++ u43/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const box = document.getElementById('box'),
+	btns = document.getElementById('btns'),
+	circles = document.getElementById('circles'),
+	hearts = document.getElementById('hearts'),
+	oneHeart = document.getElementById('oneHeart');
+
+console.dir(box);
