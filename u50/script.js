70c2e0586ed6b38f135d08bdd6c0ebcce6c4bd78
nikitos4628 <soba.nikita@mail.ru>
2023-02-19 12:07:36 +0700

    finish js course: 2 chapter (base js)

---
+++ u50/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const p = document.querySelectorAll('p');
+console.log(p);
+
+
+
+function loadScript(src) {
+	const script = document.createElement('script');
+	script.src = src;
+	script.async = false;
+	document.body.append(script);
+}
+
+loadScript('test.js');
+loadScript('some.js');
